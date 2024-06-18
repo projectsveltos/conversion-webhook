e81f9b67c401058bb8486ab49225fa5b48d4a832
mgianluc <mgianluca@cisco.com>
2024-06-18 10:36:59 +0200

    Introduce Conversion Webhook
    Conversion Webhook is used to translate from v1alpha1 to v1beta1

---
+++ main.go
@@ -0,0 +1,133 @@
+/*
+Copyright 2018 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package main
+
+import (
+	"crypto/tls"
+	"flag"
+	"os"
+
+	"github.com/spf13/pflag"
+	"k8s.io/apimachinery/pkg/runtime"
+	cliflag "k8s.io/component-base/cli/flag"
+	"k8s.io/klog/v2"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/healthz"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+	"sigs.k8s.io/controller-runtime/pkg/webhook/conversion"
+
+	configv1alpha1 "github.com/projectsveltos/addon-controller/api/v1alpha1"
+	configv1beta1 "github.com/projectsveltos/addon-controller/api/v1beta1"
+	libsveltosv1alpha1 "github.com/projectsveltos/libsveltos/api/v1alpha1"
+	libsveltosv1beta1 "github.com/projectsveltos/libsveltos/api/v1beta1"
+)
+
+var (
+	healthAddr  string
+	enableHTTP2 bool
+)
+
+// Config contains the server (the webhook) cert and key.
+type Config struct {
+	CertFile string
+	KeyFile  string
+}
+
+func main() {
+	scheme, err := initScheme()
+	if err != nil {
+		os.Exit(1)
+	}
+
+	klog.InitFlags(nil)
+
+	initFlags(pflag.CommandLine)
+	pflag.CommandLine.SetNormalizeFunc(cliflag.WordSepNormalizeFunc)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	disableHTTP2 := func(c *tls.Config) {
+		c.NextProtos = []string{"http/1.1"}
+	}
+
+	tlsOpts := []func(*tls.Config){}
+	if !enableHTTP2 {
+		tlsOpts = append(tlsOpts, disableHTTP2)
+	}
+
+	webhookServer := webhook.NewServer(webhook.Options{
+		TLSOpts: tlsOpts,
+	})
+
+	ctrl.SetLogger(klog.Background())
+	ctrlOptions := ctrl.Options{
+		Scheme:                 scheme,
+		HealthProbeBindAddress: healthAddr,
+		WebhookServer:          webhookServer,
+	}
+
+	restConfig := ctrl.GetConfigOrDie()
+
+	mgr, err := ctrl.NewManager(restConfig, ctrlOptions)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	mgr.GetWebhookServer().Register("/convert", conversion.NewWebhookHandler(mgr.GetScheme()))
+
+	ctx := ctrl.SetupSignalHandler()
+	setupChecks(mgr)
+
+	if err := mgr.Start(ctx); err != nil {
+		os.Exit(1)
+	}
+}
+
+func initFlags(fs *pflag.FlagSet) {
+	fs.StringVar(&healthAddr, "health-addr", ":9440",
+		"The address the health endpoint binds to.")
+
+	flag.BoolVar(&enableHTTP2, "enable-http2", false,
+		"If set, HTTP/2 will be enabled for the metrics and webhook servers")
+}
+
+func setupChecks(mgr ctrl.Manager) {
+	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
+		os.Exit(1)
+	}
+	if err := mgr.AddReadyzCheck("readyz", healthz.Ping); err != nil {
+		os.Exit(1)
+	}
+}
+
+func initScheme() (*runtime.Scheme, error) {
+	s := runtime.NewScheme()
+	if err := configv1beta1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := configv1alpha1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := libsveltosv1beta1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+	if err := libsveltosv1alpha1.AddToScheme(s); err != nil {
+		return nil, err
+	}
+
+	return s, nil
+}
